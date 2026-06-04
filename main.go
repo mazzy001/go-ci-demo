a2bc548361aa3ac2f684fc97bceaba3cd2291724
mazzy001 <lkkboris@gmail.com>
2026-06-04 22:11:39 +0300

    xdd

--- main.go
+++ main.go
@@ -6,13 +6,11 @@ import (
 )
 
 func main() {
-    fmt.Println("=== Демонстрация работы математического пакета ===\n")
-    
+    fmt.Println("=== Демонстрация работы математического пакета ===")
     fmt.Printf("Add(10, 5) = %d\n", math.Add(10, 5))
     fmt.Printf("IsEven(7) = %v\n", math.IsEven(7))
     fmt.Printf("IsEven(8) = %v\n", math.IsEven(8))
     fmt.Printf("Factorial(5) = %d\n", math.Factorial(5))
     fmt.Printf("Factorial(0) = %d\n", math.Factorial(0))
-    
-    fmt.Println(" Все функции работают корректно!")
+    fmt.Println("Все функции работают корректно!")
 }
